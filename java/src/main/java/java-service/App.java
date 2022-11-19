41f88a8a6be1c6185373972d65deadf4d2b8fc5e
I526519 <kiran.b.a@sap.com>
2022-11-19 11:45:28 +0530

    add sum method

--- java/src/main/java/java-service/App.java
+++ java/src/main/java/java-service/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(App.add(10, 20));
+    }
+
+    // method to add 2 numbers
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
